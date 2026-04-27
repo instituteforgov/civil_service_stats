2912f65252533c4b4e78b5cdf71e06f177c7eac6
Daniel Howes <dan.howes29@outlook.com>
2026-04-27 11:36:51 +0100

    Refine query

--- Scripts/sql/collate_age_organisations_data.sql
+++ Scripts/sql/collate_age_organisations_data.sql
@@ -39,8 +39,8 @@ o_vicd_vodg AS (
         vodg.start_quarter,
         vodg.end_year,
         vodg.end_quarter,
-        isnull(vodg.start_year * 4 + vodg.start_quarter, 0) start_period,
-        isnull(vodg.end_year * 4 + vodg.end_quarter, 2147483647) end_period
+        ISNULL(vodg.start_year * 4 + vodg.start_quarter, 0) start_period,
+        ISNULL(vodg.end_year * 4 + vodg.end_quarter, 2147483647) end_period
     FROM civil_service.organisation o 
         LEFT JOIN civil_service.vw_ifg_core_departments vicd ON
             o.id = vicd.organisation_id
@@ -50,19 +50,24 @@ o_vicd_vodg AS (
 
 SELECT 
     cs_stats_age.id,
-    cs_stats_age.year,
+    cs_stats_age.year [Year],
+    cs_stats_age.headcount [Headcount],
+    cs_stats_age.age [Age],
     cs_stats_age.organisation_name [Organisation],
     o_vicd_vodg.type [Organisation type],
     o_vicd_vodg.ifg_departmental_group_short_name [Departmental group],
     CASE
         WHEN o_vicd_vodg.is_ifg_core_department = 1 THEN 'Y'
         ELSE 'N'
-    END [IfG core department]
-
-
-FROM  cs_stats_age    
+    END [IfG core department],
+    vol1.latest_organisation_name [Latest organisation],
+    vol2.latest_organisation_short_name [Latest departmental group]
+FROM cs_stats_age    
     LEFT JOIN o_vicd_vodg ON 
-        cs_stats_age.id = o_vicd_vodg.id 
-        AND
-        (cs_stats_age.survey_period BETWEEN o_vicd_vodg.start_period AND o_vicd_vodg.end_period)
-;
+        cs_stats_age.organisation_id = o_vicd_vodg.id AND
+        cs_stats_age.survey_period BETWEEN o_vicd_vodg.start_period AND o_vicd_vodg.end_period 
+    LEFT JOIN civil_service.vw_organisation_latest vol1 ON
+        o_vicd_vodg.id = vol1.organisation_id 
+    LEFT JOIN civil_service.vw_organisation_latest vol2 ON
+        o_vicd_vodg.ifg_departmental_group_id = vol2.organisation_id
+
