c389c49b9b204c3b830dfb0c76664022884453a9
Daniel Howes <dan.howes29@outlook.com>
2026-04-30 08:37:11 +0100

    Load data and add iteration suffixes

--- Scripts/age/python/compare_age_data.py
+++ Scripts/age/python/compare_age_data.py
@@ -19,3 +19,44 @@ from utils import add_iteration_suffix
 
 # %%
 
+EXCEL_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - diversity/Civil Service - Age/Age by Department.xlsx"
+SQL_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service Statistics/Scripts/age/sql/compare_age_organisations_data.sql"
+
+# %%
+# Connect to d/b
+
+engine = dbo.connect_sql_db(
+    driver="pyodbc",
+    driver_version=os.environ["ODBC_DRIVER"],
+    dialect="mssql",
+    server=os.environ["ODBC_SERVER"],
+    database=os.environ["ODBC_DATABASE"],
+    authentication=os.environ["ODBC_AUTHENTICATION"],
+    username=os.environ["AZURE_CLIENT_ID"],
+    password=os.environ["AZURE_CLIENT_SECRET"],
+)
+
+# %%
+# Read in data
+
+df_excel = pd.read_excel(EXCEL_PATH, sheet_name="Data.Collated")
+
+with open(SQL_PATH, encoding="utf-8") as f:
+    sql = f.read()
+
+df_sql = pd.read_sql(sql, con=engine)
+
+# %%
+
+df_excel.columns
+
+# %%
+# Edit data
+
+df_excel = df_excel.drop(columns=[
+    "Managed",
+    "Census",
+    "Ministerial department/executive agency/selected non-ministerial department"
+])
+
+df_sql["Organisation"] = df_sql.apply(add_iteration_suffix, col="Organisation", axis=1)
