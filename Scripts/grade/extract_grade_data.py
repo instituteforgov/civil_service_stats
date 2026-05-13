27da5ecac1aa26653a30eea4fc782d8424f3d59f
Daniel Howes <dan.howes29@outlook.com>
2026-05-13 09:30:42 +0100

    Add file path and connect to d/b

--- Scripts/grade/extract_grade_data.py
+++ Scripts/grade/extract_grade_data.py
@@ -19,3 +19,21 @@ import os
 from sqlalchemy import NVARCHAR, SMALLINT, INT
 from sqlalchemy.dialects import UNIQUEIDENTIFIER, TINYINT
 from utils import resolve_org_id
+
+# %%
+FILE_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - grade/Grade by departmentCivil Service Age Working File.xlsx"
+
+# %%
+# Connect to d/b
+
+engine = dbo.connect_sql_db(
+    driver="pyodbc",
+    driver_version=os.environ["ODBC_DRIVER"],
+    dialect="mssql",
+    server=os.environ["ODBC_SERVER"],
+    database=os.environ["ODBC_DATABASE"],
+    authentication=os.environ["ODBC_AUTHENTICATION"],
+    username=os.environ["AZURE_CLIENT_ID"],
+    password=os.environ["AZURE_CLIENT_SECRET"],
+)
+
