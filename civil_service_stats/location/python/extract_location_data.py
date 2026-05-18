8a1404a3e04dc92e75d58b89dc70b97c82f069b0
Daniel Howes <dan.howes29@outlook.com>
2026-05-18 16:33:50 +0100

    Add imports, set constants and connect to d/b

--- civil_service_stats/location/python/extract_location_data.py
+++ civil_service_stats/location/python/extract_location_data.py
@@ -1,3 +1,4 @@
+# %%
 """
 Purpose:
     Extract Civil Service Statistics location data and write to database
@@ -9,3 +10,31 @@ Input:
 Output:
     - sql: civil_service.civil_service_statistics_location
 """
+
+import pandas as pd
+import ds_utils.database_operations as dbo
+import uuid
+import os
+
+from sqlalchemy import NVARCHAR, SMALLINT, INT
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER, TINYINT
+from civil_service_stats.utils import resolve_org_id
+
+# %%
+# Set file path constant
+
+FILE_PATH = "C:/User" + os.getlogin() + "INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - location/Location working file.xlsx"
+
+# %%
+# Establish d/b connection
+
+engine = engine = dbo.connect_sql_db(
+    driver="pyodbc",
+    driver_version=os.environ["ODBC_DRIVER"],
+    dialect="mssql",
+    server=os.environ["ODBC_SERVER"],
+    database=os.environ["ODBC_DATABASE"],
+    authentication=os.environ["ODBC_AUTHENTICATION"],
+    username=os.environ["AZURE_CLIENT_ID"],
+    password=os.environ["AZURE_CLIENT_SECRET"],
+)
