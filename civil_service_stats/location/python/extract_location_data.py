aa3db9757caf2b6d2d72ae4c4e4fd60443ae43ff
Daniel Howes <dan.howes29@outlook.com>
2026-05-18 16:30:58 +0100

    Move into subfolder and add docstring

---
+++ civil_service_stats/location/python/extract_location_data.py
@@ -0,0 +1,11 @@
+"""
+Purpose:
+    Extract Civil Service Statistics location data and write to database
+
+Input:
+    - xlsx: "Location working file.xlsx"
+        - Sheet name = Data.Collated
+
+Output:
+    - sql: civil_service.civil_service_statistics_location
+"""
