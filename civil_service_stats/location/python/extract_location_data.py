36981645ba96842d49d77f47f09b2f16e615d0ce
Daniel Howes <dan.howes29@outlook.com>
2026-05-20 12:00:02 +0100

    Edit data, add org IDs and write to d/b

--- civil_service_stats/location/python/extract_location_data.py
+++ civil_service_stats/location/python/extract_location_data.py
@@ -23,7 +23,7 @@ from civil_service_stats.utils import resolve_org_id
 # %%
 # Set file path constant
 
-FILE_PATH = "C:/Users/" + os.getlogin() + "INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - location/Location working file.xlsx"
+FILE_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - location/Location working file.xlsx"
 
 # %%
 # Establish d/b connection
@@ -37,4 +37,79 @@ engine = engine = dbo.connect_sql_db(
     authentication=os.environ["ODBC_AUTHENTICATION"],
     username=os.environ["AZURE_CLIENT_ID"],
     password=os.environ["AZURE_CLIENT_SECRET"],
+)
+
+# %%
+# Load data
+
+df_location = pd.read_excel(FILE_PATH, sheet_name="Collated.Org x region")
+
+# %%
+# Edit data
+
+# Drop calculated cols
+df_location = df_location.drop(columns=[
+    "Managed?",
+    "Release number",
+    "Departmental group",
+    "Organisation type",
+    "Managed",
+    "Census",
+    "Ministerial department/executive agency/selected non-ministerial department",
+    "Latest organisation",
+    "Latest departmental group"
+])
+
+# Add UUID column
+df_location.insert(0, 'id', [uuid.uuid4() for i in range(len(df_location))])
+
+# Edit col names
+df_location.columns = df_location.columns.str.strip().str.lower()
+df_location = df_location.rename(columns={"organisation": "organisation_name"})
+df_location["organisation_name"] = df_location["organisation_name"].str.replace(
+    r"\s*-\s*\d{4}\s*iteration\s*", "", regex=True)
+
+# %%
+# Insert organisation IDs from canonical orgs database
+
+df_orgs = pd.read_sql(
+    """select
+        o.id,
+        o.name,
+        o.start_year,
+        o.start_quarter,
+        o.end_year,
+        o.end_quarter
+    from civil_service.organisation o""",
+    engine,
+)
+
+df_location.insert(
+    df_location.columns.get_loc("organisation_name"),
+    "organisation_id",
+    resolve_org_id(df_location, df_orgs, quarter_col="quarter")
+)
+
+# %%
+df_location
+# %%
+# Write to d/b
+
+df_location.to_sql(
+    name="civil_service_statistics_location",
+    con=engine,
+    schema="civil_service",
+    if_exists="replace",
+    index=False,
+    chunksize=3000,
+    dtype={
+        "id": UNIQUEIDENTIFIER,
+        "organisation_id": UNIQUEIDENTIFIER,
+        "organisation_name": NVARCHAR(100),
+        "year": SMALLINT,
+        "quarter": TINYINT,
+        "region": NVARCHAR(30),
+        "age": NVARCHAR(20),
+        "total": INT
+    }
 )
