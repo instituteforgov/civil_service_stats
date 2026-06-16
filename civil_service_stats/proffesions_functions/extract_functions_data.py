010f8f83adc9cb18a0723b4e03e79dc6d8ebddac
Daniel Howes <dan.howes29@outlook.com>
2026-06-16 14:17:11 +0100

    Edit data

--- civil_service_stats/proffesions_functions/extract_functions_data.py
+++ civil_service_stats/proffesions_functions/extract_functions_data.py
@@ -35,4 +35,45 @@ engine = dbo.connect_sql_db(
 df_funcs = pd.read_excel(FILE_PATH, sheet_name=SHEET_NAME)
 
 # %%
+# Edit data
 
+# Drop calculated columns
+df_funcs = df_funcs.drop(columns=[
+    "Release number",
+    "Departmental group",
+    "Organisation type",
+    "Managed",
+    "Census",
+    "Ministerial department/executive agency/selected non-ministerial department",
+    "Latest organisation",
+    "Latest departmental group"
+])
+
+# Add ID col
+df_funcs.insert(0, "id", [uuid.uuid4() for _ in range(len(df_funcs))])
+
+# Edit column names
+df_funcs.columns = df_funcs.columns.str.strip().str.lower().str.replace(" ", "_")
+df_funcs = df_funcs.rename(columns={"organisation": "organisation_name", "fte": "headcount_fte"})
+df_funcs["organisation_name"] = df_funcs["organisation_name"].str.replace(r"\s*-\s*\d{4}\s*iteration\s*", "", regex=True)
+
+# %%
+# Add org ID column
+
+df_orgs = pd.read_sql(
+    """select
+        o.id,
+        o.name,
+        o.start_year,
+        o.start_quarter,
+        o.end_year,
+        o.end_quarter
+    from civil_service.organisation o""",
+    engine,
+)
+
+df_funcs.insert(
+    df_funcs.columns.get_loc("organisation_name"),
+    "organisation_id",
+    resolve_org_id(df_funcs, df_orgs, quarter_col="quarter")
+)
