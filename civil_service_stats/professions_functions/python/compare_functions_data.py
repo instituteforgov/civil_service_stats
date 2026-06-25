b5fe198e0fa54409f99936c35500cfddb6a0bcc2
Daniel Howes <dan.howes29@outlook.com>
2026-06-25 11:19:36 +0100

    Import comparison function form shared module and implement

--- civil_service_stats/professions_functions/python/compare_functions_data.py
+++ civil_service_stats/professions_functions/python/compare_functions_data.py
@@ -1,16 +1,16 @@
+# %% 
 
 import pandas as pd
 import os
 import ds_utils.database_operations as dbo
-from IPython.display import display
-
+from cs_data_utils.utils import compare_dataframes
 from civil_service_stats.utils import add_iteration_suffix
 
 # %%
 # Set filepaths
 EXCEL_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil service - professions and functions/Professions and functions of civil servants - with assumed DWP professions averages.xlsx"
 SHEET_NAME = "Data.Collated_FunctionbyDept"
-SQL_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service Statistics/Scripts/civil_service_stats/professions_functions/sql/compare_profs_organisations_data"
+SQL_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service Statistics/Scripts/civil_service_stats/professions_functions/sql/compare_funcs_organisations_data"
 
 # %%
 # Connect to DB
@@ -39,7 +39,7 @@ df_sql = pd.read_sql(sql, con=engine)
 # Edit data
 
 df_excel = df_excel.drop(columns=[
-    "Managed", "Managed?", "Census", "Ministerial department/executive agency/selected non-ministerial department"
+    "Managed", "Census", "Ministerial department/executive agency/selected non-ministerial department"
 ])
 
 df_sql["Organisation"] = df_sql.apply(add_iteration_suffix, col="Organisation", axis=1)
@@ -59,70 +59,8 @@ df_sql["Latest organisation"] = df_sql.apply(
 )
 
 # %%
-# Compare columns
-
-excel_only = [col for col in df_excel.columns if col not in df_sql.columns]
-sql_only = [col for col in df_sql.columns if col not in df_excel.columns]
-cols_both = [col for col in df_excel.columns if col in df_sql.columns]
-
-print(f"Columns in Excel frame only: {excel_only}")
-print(f"Columns in SQL frame only: {sql_only}")
-print(f"Columns in both frames: {cols_both}")
-
-# %%
-
-assert len(df_sql) == len(df_excel), f"Row count mismatch: SQL Dataframe has {len(df_sql)} rows, Excel DataFrame has {len(df_excel)}"
-
-# %%
-# Compare key column values
+# Compare dataframes
 
 key_cols = ["Year", "Quarter", "Organisation", "Function", "FTE", "Function group"]
 
-df_merge = df_sql.merge(
-    df_excel, on=key_cols, how='outer', suffixes=("_sql", "_excel"), indicator=True
-    )
-
-rows_excel = df_merge[df_merge["_merge"] == "right_only"]
-rows_sql = df_merge[df_merge["_merge"] == "left_only"]
-rows_both = df_merge[df_merge["merge"] == "both"]
-
-print(f"Rows in SQL frame only: {len(rows_sql)}")
-print(f"Rows in Excel frame only: {len(rows_excel)}")
-print(f"Rows in both: {len(rows_both)}")
-
-assert len(df_merge) == len(df_sql), f"Merged frame has {len(df_merge)} rows but SQL/Excel frames have {len(df_sql)} rows!"
-
-# %%
-# Compare values in calculated columns
-
-cols = [col for col in cols_both if col not in key_cols]
-
-mismatch_mask = {}
-for col in cols:
-    sql_col = f"{col}_sql"
-    excel_col = f"{col}_excel"
-
-    if sql_col in rows_both and excel_col in rows_both:
-        sql_series = rows_both[sql_col]
-        excel_series = rows_both[excel_col]
-        match_mask = (
-            (sql_series == excel_series) |
-            (rows_both[sql_col].isna() & rows_both[excel_col].isna())
-            )
-    if (~match_mask).any():
-        mismatch_mask[col] = (~match_mask)
-
-if mismatch_mask:
-    display({col: int(mask.sum()) for col, mask in mismatch_mask.items()})
-    for col, mask in mismatch_mask.items():
-        sql_col = f"{col}_sql"
-        excel_col = f"{col}_excel"
-        preview = (
-            rows_both.loc[mask, ["Year", "Organisation", sql_col, excel_col]]
-            .drop_duplicates()
-            .reset_index(drop=True)
-        )
-        print(f"Mismatches in '{col}':")
-        display(preview)
-else:
-    print("No value mismatches in matched rows")
+compare_dataframes(df_excel, df_sql, key_cols)
