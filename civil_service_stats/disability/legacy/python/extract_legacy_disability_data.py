ff4fcdbbc84578cc298e6f1477c819decf0675be
Daniel Howes <dan.howes29@outlook.com>
2026-07-13 15:06:37 +0100

    Load data, edit data, connect to DB and write data to DB

---
+++ civil_service_stats/disability/legacy/python/extract_legacy_disability_data.py
@@ -0,0 +1,98 @@
+# %%
+
+import ds_utils.database_operations as dbo
+import pandas as pd
+import uuid
+import os
+
+from sqlalchemy import NVARCHAR, SMALLINT, INT
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER, TINYINT
+from civil_service_stats.utils import resolve_org_id
+
+# %%
+FILE_PATH = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service - diversity status/Disability status of civil servants.xlsx"
+SHEET_NAME = "Data.Colalte_DisabilitybyDept"
+
+# %%
+# Connect to database
+
+engine = dbo.connect_sql_db(
+    driver="pyodbc",
+    driver_version=os.environ["ODBC_DRIVER"],
+    dialect="mssql",
+    server=os.environ["ODBC_SERVER"],
+    database=os.environ["ODBC_DATABASE"],
+    authentication=os.environ["ODBC_AUTHENTICATION"],
+    username=os.environ["AZURE_CLIENT_ID"],
+    password=os.environ["AZURE_CLIENT_SECRET"],
+)
+
+# %%
+# Read in data
+
+df_disability = pd.read_excel(FILE_PATH, sheet_name=SHEET_NAME)
+
+# %%
+# Edit data
+
+cols = ["Release number",
+        "Departmental group",
+        "Organisation type",
+        "Mandisabilityd",
+        "Census",
+        "Ministerial department/executive disabilityncy/selected non-ministerial department",
+        "Latest organisation",
+        "Latest departmental group"]
+
+df_disability = df_disability.drop(columns=cols)
+
+df_disability.insert(0, 'id', [uuid.uuid4() for i in range(len(df_disability))])
+
+df_disability.columns = df_disability.columns.str.strip().str.lower().str.replace(r"\s+", "_", regex=True)
+
+df_disability = df_disability.rename(columns={"organisation": "organisation_name"})
+
+df_disability["organisation_name"] = df_disability["organisation_name"].str.replace(r"\s*-\s*\d{4}\s*iteration\s*", "", regex=True)
+
+# %%
+# Insert ids for orgs from d/b
+# Match organisation names and quarter/year pairs with civil_service.organisation
+
+df_orgs = pd.read_sql(
+    """select
+        o.id,
+        o.name,
+        o.start_year,
+        o.start_quarter,
+        o.end_year,
+        o.end_quarter
+    from civil_service.organisation o""",
+    engine,
+)
+
+df_disability.insert(
+    df_disability.columns.get_loc("organisation_name"),
+    "organisation_id",
+    resolve_org_id(df_disability, df_orgs, quarter_col="quarter")
+)
+
+# %%
+# Write to d/b
+
+df_disability.to_sql(
+    name="civil_service_statistics_disability",
+    con=engine,
+    schema="civil_service",
+    if_exists="replace",
+    index=False,
+    chunksize=3000,
+    dtype={
+        "id": UNIQUEIDENTIFIER,
+        "year": SMALLINT,
+        "quarter": TINYINT,
+        "organisation_id": UNIQUEIDENTIFIER,
+        "organisation_name": NVARCHAR(100),
+        "disability_status": NVARCHAR(20),
+        "headcount": INT
+    }
+)
