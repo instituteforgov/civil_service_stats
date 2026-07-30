315e5bf0a15aead3d67a4de47b9ab819f45d9640
Daniel Howes <dan.howes29@outlook.com>
2026-07-30 16:52:51 +0100

    Set params and constants

---
+++ civil_service_stats/age/extract_age_data.py
@@ -0,0 +1,72 @@
+# %%
+"""
+    Purpose
+        Extract new CS stats age data and append to database.
+    Inputs
+        - yaml: age_params.yaml
+            - Run parameters (source file, sheet names, NA values, year)
+        - ods: 'Statistical_tables_-_Civil_Service_Statistics_<yyyy>.ods'
+            - Civil Service Statistics source file
+    Outputs
+        - sql: civil_service.civil_service_statistics_age
+            - Rows corresponding to most recent year's data appended
+    Notes
+        - New data is appended to the database table, rather than existing rows being modified
+        - Run parameters are loaded from params/releases.yaml (last entry used)
+        - Carries out the following checks on data:
+            - Structure
+                - Sheet title matches expected value
+                - Column headers match EXPECTED_COL_HEADERS
+                - First data row starts at FIRST_DATA_ROW
+            - Data quality
+                - No unused NA values
+            - Before appending
+                - No existing rows in the database for the new year
+"""
+
+import logging
+import os
+from pathlib import Path
+
+import ds_utils.database_operations as dbo
+import pandas as pd
+import yaml
+import uuid
+
+from sqlalchemy import INT, NVARCHAR, SMALLINT, text
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER, TINYINT
+from civil_service_stats.utils import resolve_org_id
+
+# %%
+# Read params
+
+with open("age_params.yamls", encoding="utf-8") as f:
+    params = yaml.safe_load(f)[-1]
+
+# %%
+# Set constants
+
+SOURCE_DIRECTORY = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service Statistics/Source"
+SOURCE_FILE = params["source_file"]
+SHEET_NAME = params["location_sheet_name"]
+EXPECTED_SHEET_TITLE = params["expected_location_sheet_title"]
+EXPECTED_YEAR = params["year"]
+NA_VALS = params["na_values"]
+
+# Define expected table layout
+HEADER_ROW = 5
+FIRST_DATA_ROW = 6
+EXPECTED_COL_NAMES = [
+    "Civil Service parent department",
+    "Civil Service organisation",
+    "Headcount of all civil servants aged between 16 and 19",
+    "Headcount of all civil servants aged between 20 and 29",
+    "Headcount of all civil servants aged between 30 and 39",
+    "Headcount of all civil servants aged between 40 and 49",
+    "Headcount of all civil servants aged between 50 and 59",
+    "Headcount of all civil servants aged between 60 and 64",
+    "Headcount of all civil servants aged 65 and over",
+    "Headcount of all civil servants with no reported age",
+    "Total headcount of all civil servants"
+]
+
