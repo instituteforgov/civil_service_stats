dd6a2456464e67ffdf023db58e4e863241761557
Daniel Howes <dan.howes29@outlook.com>
2026-07-31 12:04:25 +0100

    Set constants

---
+++ civil_service_stats/disability/extract_disability_data.py
@@ -0,0 +1,69 @@
+# %%
+"""
+    Purpose
+        Extract new CS stats disability status data and append to database.
+    Inputs
+        - yaml: disability_params.yaml
+            - Run parameters (source file, sheet names, NA values, year)
+        - ods: 'Statistical_tables_-_Civil_Service_Statistics_<yyyy>.ods'
+            - Civil Service Statistics source file
+    Outputs
+        - sql: civil_service.civil_service_statistics_disability
+            - Rows corresponding to most recent year's data appended
+    Notes
+        - New data is appended to the database table, rather than existing rows being modified
+        - Run parameters are loaded from params/releases.yaml (last entry used)
+        - Carries out the following checks on data:
+            - Structure
+                - Sheet title matches expected value
+                - Column headers match EXPECTED_COL_HEADERS
+                - First data row starts at FIRST_DATA_ROW
+            - Data quality
+                - No unused NA values
+            - Before appending
+                - No existing rows in the database for the new year
+"""
+
+import logging
+import os
+from pathlib import Path
+
+import ds_utils.database_operations as dbo
+import pandas as pd
+import yaml
+import uuid
+
+from sqlalchemy import INT, NVARCHAR, SMALLINT, text
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER, TINYINT
+from civil_service_stats.utils import resolve_org_id
+
+# %%
+# Set parameters
+
+with open("disability_params.yaml", encoding="utf-8") as f:
+    params = yaml.safe_load(f)[-1]
+
+# %%
+# Set constants
+
+SOURCE_DIRECTORY = "C:/Users/" + os.getlogin() + "/INSTITUTE FOR GOVERNMENT/Data - General/Civil service/Civil Service Statistics/Source"
+SOURCE_FILE = params["source_file"]
+SHEET_NAME = params["disability_sheet_name"]
+EXPECTED_SHEET_TITLE = params["expected_disability_sheet_title"]
+EXPECTED_YEAR = params["year"]
+NA_VALS = params["na_values"]
+
+# Define expected table layout
+HEADER_ROW = 6
+FIRST_DATA_ROW = 7
+EXPECTED_COL_NAMES = [
+    "Civil Service parent department",
+    "Civil Service organisation",
+    "Headcount of all civil servants declaring as disabled",
+    "Headcount of all civil servants declaring as non-disabled",
+    "Headcount of all civil servants actively declaring they do not want to disclose their disability status",
+    "Headcount of all civil servants who have not made an active declaration about their disability status",
+    "Total headcount of all civil servants",
+    "Headcount of all civil servants with a known disability status",
+    "Disabled civil servants as a percentage of known disability status"
+]
